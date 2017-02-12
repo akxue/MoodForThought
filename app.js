b1e8f160054f6876d203fbe869e1a181b50f0ee1
Anthony Xue <anthkx@ucla.edu>
2017-02-11 18:10:20 -0800

    get/post data on local mongodb

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 /* Use express app */
 const express = require('express');
+const bodyParser = require('body-parser');
 /* Initialize express app */
 const app = express();
-const routes = require('./routes.js')
-const mongoose = require('mongoose');
-const mongodb = require('mongodb');
+const routes = require('./routes.js');
 
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes);
+app.use('/', routes);
 
 app.listen(8080, function(){
 	console.log('App listening on 8080!')
--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 /* Set up router */
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+var url = "mongodb://localhost:27017/letters";
+
+var datab;
 
 /* Configure router to log */
 router.use(function(req, res, next){
@@ -12,5 +17,69 @@ router.get('/', function(req, res){
 	console.log(__dirname);
 	res.sendFile(__dirname + '/public/homepage.html');
 });
+
+/* When they input mood, we find json that match */
+router.get('/:mood', function(req, res){
+	var user_mood = req.params.mood;
+	//res.json({mood: user_mood});
+	var docs = []
+	findDocuments(datab, user_mood, res, function(){
+	});
+});
+
+router.get('/post/:mood-:description', function(req, res){
+	var user_mood = req.params.mood;
+	var user_desc = req.params.description;
+	res.json({description: user_desc, mood: user_mood})
+	insertDocuments(datab, user_desc, user_mood, function(){
+		indexCollection(datab, function(){});
+	});
+});
+
+/* Connect mongoClient */
+MongoClient.connect(url, function(err, db){
+	datab = db
+	// console.log(datab);
+	console.log("Connected successfully!");
+
+	// db.close();
+});
+
+var insertDocuments = function(db, user_desc, user_mood, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  //inserts one object
+  collection.insertOne(
+    {description: user_desc, mood: user_mood},
+    function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted recommendation into the collection");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, my_mood, res, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({'mood' : my_mood}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    // console.log("Found the following records");
+    // console.log(docs);
+	res.send(docs);
+    callback(docs);
+  });
+}
+
+var indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { mood: "text" },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};
 
 module.exports = router;
